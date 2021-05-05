366080b6815e24754a647fa3ae9ac2b73ac2cfb5
Benjono <56357931+Benjono@users.noreply.github.com>
2021-05-05 15:29:10 +0100

    Carousel and rendering update

--- application/js/modelInteraction.js
+++ application/js/modelInteraction.js
@@ -1,80 +1,130 @@
 // JavaScript source code
 var spinning = false;
-
+/* 
+ * Enables an animation
+ * @param {string} typeOfTimer - The type of timer (x, y or z) to enable.
+ */
 function spin(typeOfTimer) {
-    spinning = !spinning;
     var alltimers = document.querySelectorAll('[id^=model__Rot]');
     alltimers.forEach(element => element.setAttribute('enabled', 'false'));
     var timers = document.querySelectorAll('[id^=model__Rot' + typeOfTimer + ']');
     timers.forEach(element => element.setAttribute('enabled', 'true'));
 }
+/*
+ * Stops rotation of all animations
+ */
 function stopRot() {
     spinning = false;
     var timers = document.querySelectorAll('[id^=model__Rot]');
     timers.forEach(element => element.setAttribute('enabled', 'false'));
 }
+/*
+ * Legacy 
+ */
 function animateModel() {
     if (document.getElementById('model__RotZ-TIMER').getAttribute('enabled') != 'true')
         document.getElementById('model__RotZ-TIMER').setAttribute('enabled', 'true');
     else
         document.getElementById('model__RotZ-TIMER').setAttribute('enabled', 'false');
 }
+/*
+ * Changes the camera the user views the 3d model from
+ * @param {string} camera - The type of camera to focus on
+ */
 function cameraSwitch(camera) {
     var cameras = document.querySelectorAll('[id^=model__Camera' + camera + ']');
     cameras.forEach(element => element.setAttribute('set_bind', 'true'));
 }
+/*
+ * Legacy
+ */
 function cameraFront() {
     document.getElementById('model__CameraFront').setAttribute('set_bind', 'true');
 }
+/*
+ * Legacy
+ */
 function cameraBack() {
     document.getElementById('model__CameraBack').setAttribute('set_bind', 'true');
 }
+/*
+ * Legacy
+ */
 function cameraLeft() {
     document.getElementById('model__CameraLeft').setAttribute('set_bind', 'true');
 }
+/*
+ * Legacy
+ */
 function cameraRight() {
     document.getElementById('model__CameraRight').setAttribute('set_bind', 'true');
 }
+/*
+ * Legacy
+ */
 function cameraTop() {
     document.getElementById('model__CameraTop').setAttribute('set_bind', 'true');
 }
+/*
+ * Allows user to see the edges the model is constructed from
+ */
 function wireframe() {
     var x = document.querySelectorAll('[id$=Model]');
     x.forEach(element => element.runtime.togglePoints(true));
     x.forEach(element => element.runtime.togglePoints(true));
 }
+/*
+ * Allows user to see the vertices the model is constructed from
+ */
 function points() {
     var x = document.querySelectorAll('[id$=Model]');
     x.forEach(element => element.runtime.togglePoints(true));
 }
+/*
+ * Allows user to see the model
+ */
 function polygon() {
     var x = document.querySelectorAll('[id$=Model]');
     x.forEach(element => element.runtime.togglePoints(false));
     x.forEach(element => element.runtime.togglePoints(false));
 }
 var headlightOn = false;
-
+/*
+ * Turns headlight on/off
+ */
 function headlight() {
     headlightOn = !headlightOn;
     var headlights = document.querySelectorAll('[id=model__headlight]');
     headlights.forEach(element => element.setAttribute('headlight', headlightOn.toString()));
 }
-
+/*
+ * Turns omniLights on/off
+ */
 var omniLights = true;
 function omnilight() {
     omniLights = !omniLights;
     var lights = document.querySelectorAll('[id^=model__Omni]');
     lights.forEach(element => element.setAttribute('on', omniLights.toString()));
 }
+/*
+ * Turns targetted lights on/off
+ */
 var targetlightOn = true;
 function targetlight() {
-
+    targetlightOn = !targetlightOn;
+    var lights = document.querySelectorAll('[id^=model__Direct]');
+    lights.forEach(element => element.setAttribute('on', targetlightOn.toString()));
 }
+/*
+ * Resets to default settings
+ */
 function defaultSettings() {
     headlightOn = true; //True 
     headlight(); // -> false because function call
     omniLights = false;
     omnilight();
-    polygone();
+    targetlightOn = false;
+    targetlight();
+    polygon();
     spin('Z');
 }
--- application/view/mainPage.php
+++ application/view/mainPage.php
@@ -67,12 +67,39 @@
 		<div id="home" class="container-fluid main_contents">
 			<div class="row">
 				<div class="col-sm-12">
-					<div id="main_3d_image">
-						<div id="main_text" class="col-xs-12 col-sm-4">
-							<div id="title_home"><?php echo "<h2>".$data['mainTitle']['title']."</h2>"?></div>
-							<div id="subtitle_home"><?php echo "<h3>".$data['mainTitle']['subtitle']."</h3>"?></div>
-							<div id="description_home"><?php echo "<p>".$data['mainTitle']['description']."</p>"?></div>
+					<div id="theCarousel" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img class="d-block w-100" src="./application/assets/images/site_images/coke_bottle_render.jpg" alt="coke bottle" data-holder-rendered="true"/>
+								<div class="carousel-caption d-none d-md-block">
+									<h3>Coke Bottle Render</h3>
+									<p>Rendered in 3DS Max with 3 Free Photometric lights. Raycasting is used to make the glass 'glassy'</p>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="./application/assets/images/site_images/coffee_cup_render.jpg" alt="coffee cup" data-holder-rendered="true"/>
+								<div class="carousel-caption d-none d-md-block">
+									<h3>Costa Coffee Cup Render</h3>
+									<p>Rendered in 3DS Max with 3 Free Photometric lights.</p>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="./application/assets/images/site_images/fanta_can_render.jpg" alt="fanta can" data-holder-rendered="true"/>
+								<div class="carousel-caption d-none d-md-block">
+									<h3>Fanta Can Render</h3>
+									<p>Rendered in 3DS Max with 3 Free Photometric lights.</p>
+								</div>
+							</div>
+							
 						</div>
+						<a class="carousel-control-prev" href="#theCarousel" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#theCarousel" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
 					</div>
 				</div>
 			</div>
@@ -100,12 +127,18 @@
 		</div>
 		<div id="further" class="container-fluid main_contents">
 			<div class="col-sm-12">
-				<p>Use of "for ($i = 0; $i < count($data['MainBrands']); $i++){" to set up html and reduce redundant code significantly</p></br>
-				<p>Use of naming conventions (e.g. RotZ-TIMERCoke, RotZ-TIMERCosta, RotZ-TIMERFanta) to enable javascript like document.querySelectorAll('[id$=Model]'); to target all appropriate models</p></br>
-				<p>Use of multiple functions in controller to fetch all the data from different tables which is then passed to this view.</p></br>
+				<h2>Statement of originality</h2>
+				<h4>This work is my work. Has been developed solely by me except where other libraries or the teachings from the web3d module are used.</h4>
+			
+				<p>Use of "for ($i = 0; $i < count($data['MainBrands']); $i++){" to set up html and reduce redundant code significantly. Use of naming conventions (e.g. RotZ-TIMERCoke, RotZ-TIMERCosta, RotZ-TIMERFanta) to enable javascript like document.querySelectorAll('[id$=Model]'); to target all appropriate models or to implement functionality. 
+				For example, for each model it is titled 'x3dModel' and a number, then the AJAX can get the number of models, and apply functions to all x3dInfo+number id's. This code will work regardless of the number of models returned by the database</p></br>
+				<p>Use of multiple functions in controller to fetch all the data from different tables which is then passed to this view. There are more than 20 functions in the controller.</p></br>
+				<p>Use of multiple tables in the model. Including tables linked by foreign key to other tables and composite primary keys.</p></br>
 				<p>Extra use of fonts and design inspired by coca-cola. Minimalist design, should properly adjust to different screen sizes. Assumes phones need larger items as everything aligns into one column when it becomes that small</p> </br>
-				<p>Coke model particularly detailed with regular dips in model. Lid has extra detail. </p> </br>
-				<p>Attempted to use blender, as seen in _3DModelling, successfully did a model but the model won't export properly to X3D. Additionally, files are approximately 10+ times larger, which would slow loading times and consume ram</p>
+				<p>Coke model particularly detailed with regular dips in model. Lid has extra detail. Good use of specular and gloss to get 'glass-like' textures.</p> </br>
+				<p>Attempted to use blender, as seen in <a href="https://github.com/Benjono/Web-3d-Coursework/tree/main/_3DModelling">_3DModelling</a>, successfully did a model but the model won't export properly to X3D. Additionally, files are approximately 10+ times larger, which would slow loading times and consume ram</p></br>
+				<p>GITHUB link: <a href="https://github.com/Benjono/Web-3d-Coursework">https://github.com/Benjono/Web-3d-Coursework </a> </p></br>
+				<p>Link to Models specifically including assets: <a href="https://github.com/Benjono/Web-3d-Coursework/tree/main/application/assets/x3d">https://github.com/Benjono/Web-3d-Coursework/tree/main/application/assets/x3d </a> </p>
 			</div>
 		</div>
 
@@ -175,11 +208,11 @@
 							<div class="card-title x3dCamera_Subtitle drinksText">
 								<h3>Camera Views</h3>
 							</div>
-							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Front');"><i class="fas fa-undo fa-2x"></i></a>
-							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Back');"><i class="fas fa-angle-down fa-2x"></i></a>
-							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Left');"><i class="fas fa-angle-left fa-2x"></i></a>
-							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Right');"><i class="fas fa-angle-right fa-2x"></i></a>
-							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Top');"><i class="fas fa-angle-up fa-2x"></i></a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Front');" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Front"><i class="fas fa-undo fa-2x"></i></a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Back');" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Back"><i class="fas fa-angle-down fa-2x"></i></a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Left');" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Left"><i class="fas fa-angle-left fa-2x"></i></a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Right');" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Right"><i class="fas fa-angle-right fa-2x"></i></a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="cameraSwitch('Top');" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Top"><i class="fas fa-angle-up fa-2x"></i></a>
 							<div class="card-text x3dCameraDescription drinksText">
 								<p>These buttons select a limited range of X3D model viewpoints, use the dropdown menu for more camera views</p>
 							</div>
@@ -200,8 +233,8 @@
 								<h3>Animation Options</h3>
 							</div>
 							<a href="#" class="btn btn-outline-dark btn-responsive btn-anim" onclick="spin('X');">X</a>
-							<a href="#" class="btn btn-outline-dark btn-responsive btn-anim" onclick="spin('Y');">Y</a>
-							<a href="#" class="btn btn-outline-dark btn-responsive btn-anim" onclick="spin('Z');">Z</a>
+							<a href="#" class="btn btn-outline-dark btn-responsive btn-anim" onclick="spin('Y');">Z</a>
+							<a href="#" class="btn btn-outline-dark btn-responsive btn-anim" onclick="spin('Z');">Y</a>
 							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="stopRot();"><i class="fas fa-ban fa-2x"></i></a>
 							<div class="card-text x3dAnimationDescription drinksText">
 								<p>These buttons select a range of X3D animation options</p>
@@ -224,9 +257,9 @@
 								<li class="nav-item dropdown">
 									<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Lights</a>
 									<div class="dropdown-menu">
-										<a class="dropdown-item" href="#" onclick="cameraFront();">Default</a>
+										<a class="dropdown-item" href="#" onclick="defaultSettings();">Default</a>
 										<a class="dropdown-item" href="#" onclick="omnilight();">Omni On/Off</a>
-										<a class="dropdown-item" href="#" onclick="cameraLeft();">Target On/Off</a>
+										<a class="dropdown-item" href="#" onclick="targetlight();">Target On/Off</a>
 										<a class="dropdown-item" href="#" onclick="headlight();">Headlight On/Off</a>
 									</div>
 								</li>
@@ -239,7 +272,7 @@
 							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="polygon();">Poly</a>
 							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="wireframe();">Wire</a>
 							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="headlight();">Headlight</a>
-							<a href="#" class="btn btn-outline-dark btn-responsive">Default</a>
+							<a href="#" class="btn btn-outline-dark btn-responsive" onclick="defaultSettings();">Default</a>
 							<div class="card-text x3dRenderDescription drinksText">
 								<p>These buttons select a limited number of render and lighting options; use the dropdown menus for more options</p>
 							</div>
@@ -304,7 +337,7 @@
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="./application/js/bootstrap.js"></script>
+	<script src="./application/js/bootstrap.min.js"></script>
 	<script src="https://kit.fontawesome.com/2d506ca9cf.js" crossorigin="anonymous"></script>
 	<script src="./application/js/custom.js"></script>
 	<!-- X3dom -->
